5508db911bf72d14182601f975fe68b7816b0d9b
Alex Gray <alex@mrgray.com>
2014-06-13 09:57:41 -0400

    various minor enhancements

--- AQOptionParser/AQOptionRequirementGroup.m
+++ AQOptionParser/AQOptionRequirementGroup.m
@@ -1,76 +1,51 @@
-//
-//  AQOptionRequirementGroup.m
-//  AQOptionParser
-//
-//  Created by Jim Dovey on 2012-05-23.
-//  Copyright (c) 2012 Jim Dovey. All rights reserved.
-//
 
-#import "AQOptionRequirementGroup.h"
+//  AQOptionRequirementGroup.m AQOptionParser  Created by Jim Dovey on 2012-05-23.  Copyright (c) 2012 Jim Dovey. All rights reserved.
+
 #import "AQOption_Internal.h"
-#import "AQOptionRequirementGroup_Internal.h"
 
 NSString * const AQOptionExclusiveRequirementMultipleMatchException = @"AQOptionExclusiveRequirementMultipleMatchException";
 
 @implementation AQOptionRequirementGroup
 {
-    NSOrderedSet *          _options;
-    AQOptionRequirementType _type;
-    AQOption * __weak       _firstMatched;
+  AQOption * __weak       _firstMatched;
 }
 
 @synthesize options=_options, type=_type;
 
-- (id) initWithOptions: (NSArray *) options type: (AQOptionRequirementType) type
+- (id) initWithOptions:(NSArray*) options type: (AQOptionRequirementType) type
 {
-    self = [super init];
-    if ( self == nil )
-        return ( nil );
-    
-    _type = type;
-    _options = [NSOrderedSet orderedSetWithArray: options];
-    for ( AQOption * option in _options )
-    {
-        [option addedToRequirementGroup: self];
-    }
-    
-    return ( self );
+  if (!(self = super.init)) return nil;
+  _type     = type;
+  _options  = [NSOrderedSet orderedSetWithArray: options];
+  for (AQOption * option in _options) [option addedToRequirementGroup: self];
+  return self;
 }
 
-- (BOOL) containsOption: (AQOption *) option
-{
-    return ( [_options containsObject: option] );
-}
+- (BOOL) containsOption:(AQOption*)option { return [_options containsObject: option]; }
 
-- (BOOL) isMatched
-{
-    return ( _firstMatched != nil );
-}
+- (BOOL) isMatched { return !!_firstMatched; }
 
-- (NSString *) description
+- (NSString*) description
 {
-    return ( [NSString stringWithFormat: @"%@: %s, options=%@", [super description], (_type == AQOptionRequirementAtLeastOne ? "any" : "exclusive"), _options] );
+  return [NSString stringWithFormat: @"%@: %s, options=%@", [super description],_type == AQOptionRequirementAtLeastOne ? "any" : "exclusive", _options];
 }
 
 - (NSUInteger) hash
 {
-    return ( [_options hash] );
+  return _options.hash;
 }
 
 @end
 
 @implementation AQOptionRequirementGroup (Internal)
 
-- (void) optionWasMatched: (AQOption *) option
+- (void) optionWasMatched: (AQOption*) option
 {
-    if ( _type == AQOptionRequirementExclusive && _firstMatched != option )
-    {
-        // already matched something else, so the exclusive requirement fails
-        [NSException raise: AQOptionExclusiveRequirementMultipleMatchException format: @"Matched mutually exclusive options %@ and %@", _firstMatched, option];
-        return;
-    }
-    
-    _firstMatched = option;
+  if ( _type == AQOptionRequirementExclusive && _firstMatched != option )
+    // already matched something else, so the exclusive requirement fails
+    return [NSException raise: AQOptionExclusiveRequirementMultipleMatchException format: @"Matched mutually exclusive options %@ and %@", _firstMatched, option];
+
+  _firstMatched = option;
 }
 
 @end
--- AQOptionParserTests/AQOptionParserTests.m
+++ AQOptionParserTests/AQOptionParserTests.m
@@ -1,109 +1,87 @@
-//
-//  AQOptionParserTests.m
-//  AQOptionParserTests
-//
-//  Created by Jim Dovey on 2012-05-23.
-//  Copyright (c) 2012 Jim Dovey. All rights reserved.
-//
 
-#import "AQOptionParserTests.h"
+//  AQOptionParserTests.m  AQOptionParserTests  Created by Jim Dovey on 2012-05-23. Copyright (c) 2012 Jim Dovey. All rights reserved.
+
+#import <SenTestingKit/SenTestingKit.h>
 #import <AQOptionParser/AQOptionParser.h>
 #import <getopt.h>
 
+@interface      AQOptionParserTests : SenTestCase
+{
+  NSError *__autoreleasing *error; AQOption * option; AQOptionParser * parser; int argc;
+}
+@end
 @implementation AQOptionParserTests
 
-- (void) tearDown
-{
-    optreset = 1;
-    optind = 1;
-    [super tearDown];
-}
+- (void) setUp    { [super    setUp];    parser = AQOptionParser.new; error = nil; }
+- (void) tearDown { [super tearDown];  optreset = 1; optind = 1;      }
 
 - (void) testShortOptionSpacedMatching
 {
-    AQOption * option = [[AQOption alloc] initWithLongName: @"test-attr" shortName: 't' requiresParameter: AQOptionParameterTypeRequired optional: NO];
-    AQOptionParser * parser = [AQOptionParser new];
-    [parser addOption: option];
-    
-    int argc = 3;
-    char * argv[] = { "testerApp", "-t", "value", NULL };
-    
-    NSError * error = nil;
-    XCTAssertTrue([parser parseCommandLineArguments: argv count: argc nextArgumentIndex: NULL error: &error], @"Expected parser to succeed");
-    XCTAssertTrue(option.matched, @"Expected option to match with short option specified");
-    XCTAssertEqualObjects(option.parameter, @"value", @"Expected matched value to be 'value', but got %@", option.parameter);
+  option = [parser addOption:[AQOption optionWithLName: @"test-attr" sName: 't' requires: AQOptionParameterTypeRequired opt: NO]];
+
+  argc = 3; char *argv[] = { "testerApp", "-t", "value", NULL };
+
+          STAssertTrue([parser parseCommandLineArguments: argv count: argc nextArgumentIndex: NULL error:error], @"Expected parser to succeed");
+          STAssertTrue(option.matched, @"Expected option to match with short option specified");
+  STAssertEqualObjects(option.parameter, @"value", @"Expected matched value to be 'value', but got %@", option.parameter);
 }
 
 - (void) testShortOptionNonSpacedMatching
 {
-    AQOption * option = [[AQOption alloc] initWithLongName: @"test-attr" shortName: 't' requiresParameter: AQOptionParameterTypeRequired optional: NO];
-    AQOptionParser * parser = [AQOptionParser new];
-    [parser addOption: option];
-    
-    int argc = 2;
-    char * argv[] = { "testerApp", "-tvalue", NULL };
-    
-    NSError * error = nil;
-    XCTAssertTrue([parser parseCommandLineArguments: argv count: argc nextArgumentIndex: NULL error: &error], @"Expected parser to succeed");
-    XCTAssertTrue(option.matched, @"Expected option to match with short option specified");
-    XCTAssertEqualObjects(option.parameter, @"value", @"Expected matched value to be 'value', but got %@", option.parameter);
+  option = [parser addOption:[AQOption optionWithLName:@"test-attr" sName: 't' requires:AQOptionParameterTypeRequired opt: NO]];
+
+  argc = 2; char * argv[] = { "testerApp", "-tvalue", NULL };
+
+          STAssertTrue([parser parseCommandLineArguments: argv count: argc nextArgumentIndex: NULL error:error], @"Expected parser to succeed");
+          STAssertTrue(option.matched, @"Expected option to match with short option specified");
+  STAssertEqualObjects(option.parameter, @"value", @"Expected matched value to be 'value', but got %@", option.parameter);
 }
 
 - (void) testLongOptionEqualsMatching
 {
-    AQOption * option = [[AQOption alloc] initWithLongName: @"test-attr" shortName: 't' requiresParameter: AQOptionParameterTypeRequired optional: NO];
-    AQOptionParser * parser = [AQOptionParser new];
-    [parser addOption: option];
-    
-    int argc = 2;
-    char * argv[] = { "testerApp", "--test-attr=the value", NULL };
-    NSError * error = nil;
-    XCTAssertTrue([parser parseCommandLineArguments: argv count: argc nextArgumentIndex: NULL error: &error], @"Expected parser to succeed");
-    XCTAssertTrue(option.matched, @"Expected option to match with long option specified");
-    XCTAssertEqualObjects(option.parameter, @"the value", @"Expected matched value to be 'the value', but got %@", option.parameter);
+  option = [parser addOption:[AQOption optionWithLName:@"test-attr" sName: 't' requires: AQOptionParameterTypeRequired opt: NO]];
+
+  argc = 2; char * argv[] = { "testerApp", "--test-attr=the value", NULL };
+
+          STAssertTrue([parser parseCommandLineArguments: argv count: argc nextArgumentIndex: NULL error: error], @"Expected parser to succeed");
+          STAssertTrue(option.matched, @"Expected option to match with long option specified");
+  STAssertEqualObjects(option.parameter, @"the value", @"Expected matched value to be 'the value', but got %@", option.parameter);
 }
 
 - (void) testLongOptionSpacedMatching
 {
-    AQOption * option = [[AQOption alloc] initWithLongName: @"test-attr" shortName: 't' requiresParameter: AQOptionParameterTypeRequired optional: NO];
-    AQOptionParser * parser = [AQOptionParser new];
-    [parser addOption: option];
-    
-    int argc = 3;
-    char * argv[] = { "testerApp", "--test-attr", "the value", NULL };
-    NSError * error = nil;
-    XCTAssertTrue([parser parseCommandLineArguments: argv count: argc nextArgumentIndex: NULL error: &error], @"Expected parser to succeed");
-    XCTAssertTrue(option.matched, @"Expected option to match with long option specified");
-    XCTAssertEqualObjects(option.parameter, @"the value", @"Expected matched value to be 'the value', but got %@", option.parameter);
+  option = [parser addOption:[AQOption optionWithLName:@"test-attr" sName: 't' requires: AQOptionParameterTypeRequired opt: NO]];
+
+  argc = 3; char * argv[] = { "testerApp", "--test-attr", "the value", NULL };
+
+          STAssertTrue([parser parseCommandLineArguments: argv count: argc nextArgumentIndex: NULL error:error], @"Expected parser to succeed");
+          STAssertTrue(option.matched, @"Expected option to match with long option specified");
+  STAssertEqualObjects(option.parameter, @"the value", @"Expected matched value to be 'the value', but got %@", option.parameter);
 }
 
 - (void) testSingleOptionLocalizedDescription
 {
-    AQOption * option = [[AQOption alloc] initWithLongName: @"test-attr" shortName: 't' requiresParameter: AQOptionParameterTypeRequired optional: NO];
-    option.localizedUsageInformation = @{ AQOptionUsageLocalizedDescription : NSLocalizedString(@"A very nice option with which to test this library", @""), AQOptionUsageLocalizedValueName : @"{test}" };
-    AQOptionParser * parser = [AQOptionParser new];
-    [parser addOption: option];
-    
-    NSString * usageInfo = [parser localizedUsageInformationWithColumnWidth: 0];
-    NSString * wrappedUsageInfo = [parser localizedUsageInformationWithColumnWidth: 40];
-    
-    NSString * expectedPlain = @"  --test-attr={test} | t {test}:\n    A very nice option with which to test this library.\n";
-    NSString * expectedWrapped = @"  --test-attr={test} | t {test}:\n    A very nice option with which to \n    test this library.\n";
-    
-    XCTAssertEqualObjects(usageInfo, expectedPlain, @"Expected '%@' but got '%@'", expectedPlain, usageInfo);
-    XCTAssertEqualObjects(wrappedUsageInfo, expectedWrapped, @"Expected '%@' but got '%@'", expectedWrapped, wrappedUsageInfo);
+  option = [parser addOption: [AQOption optionWithLName:@"test-attr" sName: 't' requires: AQOptionParameterTypeRequired opt: NO]];
+  option.localizedUsageInformation = @{ AQOptionUsageLocalizedDescription : NSLocalizedString(@"A very nice option with which to test this library", @""), AQOptionUsageLocalizedValueName : @"{test}" };
+
+  NSString * usageInfo = [parser localizedUsageInformationWithColumnWidth: 0],
+    * wrappedUsageInfo = [parser localizedUsageInformationWithColumnWidth: 40],
+       * expectedPlain = @"  --test-attr={test} | t {test}:\n    A very nice option with which to test this library.\n",
+     * expectedWrapped = @"  --test-attr={test} | t {test}:\n    A very nice option with which to \n    test this library.\n";
+
+  STAssertEqualObjects(usageInfo,          expectedPlain, @"Expected '%@' but got '%@'", expectedPlain, usageInfo);
+  STAssertEqualObjects(wrappedUsageInfo, expectedWrapped, @"Expected '%@' but got '%@'", expectedWrapped, wrappedUsageInfo);
 }
 
 - (void) testHyphenationOfLocalizedDescription
 {
-    AQOption * option = [[AQOption alloc] initWithLongName: @"test-attr" shortName: 't' requiresParameter: AQOptionParameterTypeRequired optional: NO];
-    option.localizedUsageInformation = @{ AQOptionUsageLocalizedDescription : NSLocalizedString(@"A localized descriptive dedication of eloquently assembled linguistics in sequence.", @""), AQOptionUsageLocalizedValueName : @"{test}" };
-    AQOptionParser * parser = [AQOptionParser new];
-    [parser addOption: option];
-    
-    NSString * usage = [parser localizedUsageInformationWithColumnWidth: 40];
-    NSString * expected = @"  --test-attr={test} | t {test}:\n    A localized descriptive dedication\n    of eloquently assembled linguis-\n    tics in sequence.\n";
-    XCTAssertEqualObjects(usage, expected, @"Expected '%@' but got '%@'", expected, usage);
+  option = [parser addOption:[AQOption optionWithLName:@"test-attr" sName: 't' requires: AQOptionParameterTypeRequired opt: NO]];
+  option.localizedUsageInformation = @{ AQOptionUsageLocalizedDescription : NSLocalizedString(@"A localized descriptive dedication of eloquently assembled linguistics in sequence.", @""), AQOptionUsageLocalizedValueName : @"{test}" };
+
+  NSString * usage = [parser localizedUsageInformationWithColumnWidth: 40],
+        * expected = @"  --test-attr={test} | t {test}:\n    A localized descriptive dedication\n    of eloquently assembled linguis-\n    tics in sequence.\n";
+
+  STAssertEqualObjects(usage, expected, @"Expected '%@' but got '%@'", expected, usage);
 }
 
 @end
